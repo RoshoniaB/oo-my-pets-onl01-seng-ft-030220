ef03807a2de7357ee391a8727cecf3f993f73f5b
Roshonia Brooks <roshoniabrooks@gmail.com>
2020-03-15 04:42:46 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,4 +1,13 @@
 class Cat
 attr_accessor :mood, :owner
+ 
+ 
+ @@all = []
 
+  def initialize(name, owner)
+    @name = name
+    @mood = "nervous"
+    @owner = owner
+    @@all << self
+  end
 end
