f81e4a745a8ad655e4a424bfb8465ab31562fd70
Roshonia Brooks <roshoniabrooks@gmail.com>
2020-03-15 04:44:57 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,5 +1,16 @@
 class Dog
  attr_accessor :mood, :owner
  
- 
+  @@all = []
+
+  def initialize(name, owner)
+    @name = name
+    @mood = "nervous"
+    @owner = owner
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
 end
